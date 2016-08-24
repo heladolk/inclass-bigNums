c5fcf34684ce4be459d17e2f9e7bbad8406d25e3
heladolk <heladolk@yahoo.com>
2016-08-24 11:13:29 -0700

    add solutions

--- js/controllers.js
+++ js/controllers.js
@@ -2,4 +2,5 @@ var testApp = angular.module('testApp', []);
 
 testApp.controller('TestController', function ($scope) {
 	$scope.bigNums = [34345, 234532, 3454, 236346, 23434545, 23]
+	$scope.num = 10000.00000;
 });
