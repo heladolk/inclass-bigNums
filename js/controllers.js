ffee501663eef13358fa2941bcd4bddf668a01bc
heladolk <heladolk@yahoo.com>
2016-08-24 11:25:20 -0700

    refactor

--- js/controllers.js
+++ js/controllers.js
@@ -1,6 +1,6 @@
 var testApp = angular.module('testApp', []);
 
 testApp.controller('TestController', function ($scope) {
-	$scope.bigNums = [34345, 234532, 3454, 236346, 23434545, 23]
+	$scope.bigNums = [34345, 234532, 3454, 236346, 23434545, 23, 23]
 	$scope.num = 10000.00000;
 });
